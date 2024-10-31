d31a2f6094037d257d65d64e00420693348427c6
Toluwalase Aro <arodiana2019@gmail.com>
2024-10-31 10:39:48 +0100

    HPlusSport

--- HPlusSport.API/Models/Product.cs
+++ HPlusSport.API/Models/Product.cs
@@ -1,15 +1,19 @@
 using System.Text.Json.Serialization;
 
-namespace HPlusSport.AI.Models{
-    public class ProductsController{
+namespace HPlusSport.API.Models
+{
+    public class Product
+    {
         public int Id { get; set; }
         public string Sku { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public bool IsAvailable { get; set; }
         public int CategoryId { get; set; }
+
         [JsonIgnore]
-        public virtual Category { get; set; }
+        public virtual Category Category { get; set; } = new Category();
     }
 }
+
