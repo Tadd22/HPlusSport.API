13cadfbbf150226b650ef2e33d30ac09e54dcc15
Toluwalase Aro <arodiana2019@gmail.com>
2024-10-31 09:55:48 +0100

    first commit

---
+++ Models/Products.cs
@@ -0,0 +1,15 @@
+using System.Text.Json.Serialization;
+
+namespace HPlusSport.AI.Models{
+    public class ProductsController{
+        public int Id { get; set; }
+        public string Sku { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set } = string.Empty;
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public int CategoryId { get; set; }
+        [JsonIgnore]
+        public virtual Category { get; set; }
+    }
+}
